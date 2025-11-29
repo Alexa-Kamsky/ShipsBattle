5c44171bf52d3207274cc1ab202bdbff29c0673d
Alexa Kamsky <146553204+Alexa-Kamsky@users.noreply.github.com>
2025-11-29 17:38:56 +0300

    Добавление "Запрос на начало игры у пользователя"
    Функция запрашивает у пользователя ответ на вопрос, хочет ли он начать игру. Если ответ положительный (д, да, y, yes) - программа продолжает выполняться. Если отрицательный (н, нет, n, no) - программа прекращает работ. Если пользователь не ввел ничего - программа выходит в вечный цикл, пока не получит или позитивный, или отрицательный ответ.

--- Program.cs
+++ Program.cs
@@ -16,4 +16,27 @@ class ShipBattle
                 "Если у противника имеется корабль с этими координатами, то корабль или его палуба (дека) поражается, попавший делает ещё один ход." +
                 "Цель игрока: первым уничтожить все корабли противника. (дописать в конце)\n");
   }
+
+  static bool CheckUserAnswer()
+  {
+      string answerUser = ReadLine().Trim().ToLower();
+  
+      string[] positiveAnswers = { "да", "д", "yes", "y" };
+      string[] negativeAnswers = { "нет", "н", "no", "n" };
+  
+      if (positiveAnswers.Contains(answerUser))
+      {
+          return true;
+      }
+      else if (negativeAnswers.Contains(answerUser))
+      {
+          return false;
+      }
+      else
+      {
+          WriteLine("Некорректный ответ. Попроуйте снова:");
+          return CheckUserAnswer();
+      }
+  }
+  
 }
